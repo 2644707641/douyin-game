fbccd1754c27034c556c2a7b47a2ddd4b22acef8
2644707641 <2644707641@qq.com>
2026-07-05 03:10:21 +0800

    fix: 加载旧存档后强制清空道具数据 (_props = [])

--- build-ctx/html/js/bundle.js
+++ build-ctx/html/js/bundle.js
@@ -1107,7 +1107,7 @@
                         void 0 !== t[s][i] && (this._data[s][i] = t[s][i])
                     }) : Array.isArray(t[s]) && (this._data[s] = t[s]) : void 0 !== t[s] && (this._data[s] = t[s])
                 }
-                this.resetData(), this.setData()
+                this._data._props = [], this.resetData(), this.setData()
             }
             getType(t) {
                 var s = Object.prototype.toString.call(t).match(/^\[object (.*)\]$/)[1].toLowerCase();
